07f36bbd537a8ea56e14f3305b817571287f8eda
SaleamlakH <saleamlak.7h@gmail.com>
2026-01-12 09:50:16 +0300

    Feat: Add function to instantiate and store books

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 'use strict'
+let myLibrary = [];
 
-function Book(title, author, numPages) {
+function Book(title, author, numPages, uuid) {
     if (!new.target) {
         throw Error('You must use new operator to call as a constructor');
     }
     this.title = title;
     this.author = author;
     this.pages = numPages;
+    this.uuid = uuid;
 }
+
+function addBookToLibrary(title, author, numPages) {
+    const uuid = crypto.randomUUID();
+    const newBook = new Book(title, author, numPages, uuid);
+    
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295);
